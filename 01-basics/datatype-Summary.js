54689edb0c96c5403922681377370330518f4f39
Ankul yadav <ankuly297@gmail.com>
2026-03-07 16:18:28 +0000

    data

---
+++ 01-basics/datatype-Summary.js
@@ -0,0 +1,37 @@
+                               //Type of Data Type //
+
+
+//  primitive
+
+// 7 Type : String , Number , Boolean , Null , Undefined , symbol , bigint
+
+
+//const score = 100
+//const scoreValue = 100.3
+//const isLoggedIn = false
+//const outsideTemp = null
+let   userEmail;
+
+const id = symbol('1234')
+const  anotherId =symbol ('1234')
+
+console.log (id === anotherId) ;
+
+const bigNumber =1235456789976533n
+
+const heros = ["shaktiman","naagraj","doga",]     
+let myobj = {
+    name:"ankul",                                
+    age : 22 
+}
+const myFunction = function(){
+    console.log ("hello World");
+}
+
+console.log(typeof heros)
+
+
+
+// Referance(Non primitive)
+
+// Array , Objects , functions 
