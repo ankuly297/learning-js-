ec73fe77a6c838571a99169e70e0292e63cece23
Ankul yadav <ankuly297@gmail.com>
2026-03-07 12:32:52 +0000

    add me

--- 01-basics/03-datatypeConversion.js
+++ 01-basics/03-datatypeConversion.js
@@ -23,7 +23,46 @@
 //""=> false ;
 //"ankul" => true
 
-let someNumber = 55
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+//let someNumber = 55
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber)
+
+
+
+
+///*************************OPerations*********************************///
+let value = 3
+let negvalue = -value
+
+
+
+//console.log( negvalue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(3*3)
+//console.log(2**3)
+//console.log(2/3 )
+//console.log(2%3)
+
+//let str1 = "hello"
+//let str2 = " ankul"
+//console.log( str1+str2)
+
+
+//console.log ("1"+2)
+//console.log ( 1+"2")
+//console.log ("1"+2+2)
+//console.log(1+2+"2") 
+
+console.log (+true)
+console.log(+"")
+
+    let   num1,num2,num3
+  
+    num1 = num2,num3 = 2+2
+
+
+    let gameCounter =100
+    gameCounter ++;
+    console.log (gameCounter)
