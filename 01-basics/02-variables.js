d3a60438cde0b1bdab642e72d45471342d80ec55
Ankul yadav <ankuly297@gmail.com>
2026-03-07 09:47:18 +0000

    push

---
+++ 01-basics/02-variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "ddhfgfjf@gmail.com" // let change in future in any time and defin the differt value //
+var accountPassword ="1222222"
+accountCity = "jaipur"
+let accountState ;
+
+//     const accountId =2      const can`t change the value once define//
+
+accountEmail= " gggggg@mgmail.com"
+accountPassword ="55555"
+accountCity = " goa"
+
+/* preffeer not to use var
+because of issue in block scope and funcation scope 
+*/
+
+ 
+console.table ([accountEmail,accountId,accountPassword,accountCity, accountState])
