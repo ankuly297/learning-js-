21bbfca6bb3fce77ac7f84b115d7b038e123d2df
Ankul yadav <ankuly297@gmail.com>
2026-03-07 10:40:03 +0000

    no

---
+++ 01-basics/03-datatype.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code newer version//
+
+  // alert(3+3)  // we are using nodejs, not a browser
+
+  console.log(3+3) // this is good pratices//
+   
+  console.log(3+
+    3           // this is not a good pratices
+  )             //code readability should is high
+
+
+  let name = "ankul "
+  let age = "21"
+  let isLoggedIn = false
+
+
+
+
+  // number=> 2 to power 53
+  //bigint
+  //string => " "
+  //boolean => true/False
+  //null => standalosne value   // empity value
+  //undefined => Value is not deffined
+  //symbol => unique
+
+
+  console.log( typeof age)
+  
