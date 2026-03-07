41226d78aafbe2d0e8dfd2e0c94b9227ef66c37a
Ankul yadav <ankuly297@gmail.com>
2026-03-07 13:00:18 +0000

    comparison

---
+++ 01-basics/04-comparisonOfdatatype.js
@@ -0,0 +1,17 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+
+console.log("2">1)
+console.log("02">1)
+
+console.log( null>0)
+console.log(null==0)
+console.log(null>=0)
+
+
+/////=== they check strickly they do not convert ,only check
+console.log("2"==2)
