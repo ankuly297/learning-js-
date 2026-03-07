63bead55c7ecec85db77551f7b0269ba005c5abd
Ankul yadav <ankuly297@gmail.com>
2026-03-07 11:57:17 +0000

    conversion

---
+++ 01-basics/03-datatypeConversion.js
@@ -0,0 +1,29 @@
+//let score = "true"
+
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//let valueInNumner= Number(score)
+//console.log( typeof  valueInNumner)
+//console.log( valueInNumner)
+
+
+// "33"=> number
+//"33abc" => not a number
+//true => 1; false => 0
+
+
+//let isLoggedIn = ""
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+
+//1 => true  ; 0 => false
+//""=> false ;
+//"ankul" => true
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
