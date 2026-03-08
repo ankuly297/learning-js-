04dd87faf8496727776e8df4a107272650fca668
Ankul yadav <ankuly297@gmail.com>
2026-03-08 18:05:23 +0000

    function

---
+++ 03-basics/01-functions.js
@@ -0,0 +1,23 @@
+function sayMyName (){
+    console.log("k")
+    console.log("l" )
+    console.log("M") 
+
+}
+//sayMyName()
+
+//function addTwoNumbers(number1,number2){
+//console.log (number1 +number2)
+
+//addTwoNumbers (3,4)
+
+function addTwoNumbers( number1,number2){
+
+   // let result = number1 +number2
+    //return result
+     //return number1 +number2
+}
+const result=addTwoNumbers(3,5)
+console.log("Result:",result)
+
+
