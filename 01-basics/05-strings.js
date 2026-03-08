deafc092c71a6a1725ccf09529f5586c97df0cc9
Ankul yadav <ankuly297@gmail.com>
2026-03-08 06:54:30 +0000

    Strings

--- 01-basics/05-strings.js
+++ 01-basics/05-strings.js
@@ -1,5 +1,33 @@
-//const name ="ankul"
-//const repoCount = 50 
+const name ="ankul"
+const repoCount = 50 
 
-//console.log( name +  repoCount + "value")
-console.log (`hello my name is ${ankul}and my repo count is ${ repoCount}`);
+console.log( name +  repoCount + "value")
+
+console.log ( `hello my name is ${name} and my repoCount is ${ repoCount}`)
+
+const gameName = new String ('ankula')
+
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase ())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('u'))
+
+const newString = gameName.substring(-8, 3)
+console.log(newString)
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString)
+
+const newStringone = "     ankul   "
+console.log(newStringone)
+console.log(newStringone.trim())
+
+const url = "https://ankul.com/ankul%20yadav"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('_'))
