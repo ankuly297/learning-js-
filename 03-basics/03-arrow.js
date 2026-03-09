5623535b837b6424ff1c3083a7e75b144a4e1c08
Ankul yadav <ankuly297@gmail.com>
2026-03-09 07:39:46 +0000

    arrow

---
+++ 03-basics/03-arrow.js
@@ -0,0 +1,44 @@
+const user = {
+    username :"ankul",
+    price : 999,
+
+  welcomeMessage : function(){
+    console.log(`${this.username},welcome to my Website`)
+    console.log(this)  
+}
+
+
+}
+//user.welcomeMessage()
+//user.username ="sam"
+//user.welcomeMessage()
+console.log(this)
+
+//function chai(){
+  //  console.log(this.username)
+//}
+//chai()
+
+//const chai = function(){
+ //   let username ="gulshan"
+   // console.log(this.username)
+//}
+
+//const chai = ()=>{
+  //  let username ="gulshan"
+   // console.log(this.)
+//}
+//chai()
+
+//const addTwo = (num1 , num2 )=>{
+  //  return num1 + num2
+//}
+
+//const addTwo = (num1 , num2 )=>  num1 + num2
+//const addTwo = (num1 , num2 )=>  (num1 + num2)
+//const addTwo = (num1 , num2 )=>  ({username:"ankul"})
+
+
+
+
+console.log(addTwo(3,4))
