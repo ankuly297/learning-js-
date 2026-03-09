9bcb1fed0db8a5c6bf15e86e3eed5cbc487378ab
Ankul yadav <ankuly297@gmail.com>
2026-03-09 06:01:45 +0000

    scope

---
+++ 03-basics/02-scope.js
@@ -0,0 +1,16 @@
+//var c = 300
+let a =320
+
+//console.log(a)
+//console.log(B)
+//console.log(C)
+
+if(true){
+
+    let a = 10
+    const B = 20
+     console.log("INNER:",a)
+}
+console.log(a)
+//console.log(B)
+//console.log(C)
