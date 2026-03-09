b84b09a30ae4bb5c4db27fee6a422d5623508c47
Ankul yadav <ankuly297@gmail.com>
2026-03-09 05:43:33 +0000

    function

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -13,11 +13,45 @@ function sayMyName (){
 
 function addTwoNumbers( number1,number2){
 
-   // let result = number1 +number2
-    //return result
-     //return number1 +number2
+   let result = number1 +number2
+    return result
+    return number1 +number2
 }
 const result=addTwoNumbers(3,5)
-console.log("Result:",result)
+//console.log("result:",result)
 
 
+function loginUserMessage (username){
+    return `${username}just logged in`
+}
+console.log(loginUserMessage("ankul"))
+
+
+
+function calculateCartPrice( ...num1){
+    return num1
+}
+console.log(calculateCartPrice(2,200,300,400,500))
+
+
+
+const user={
+    username: "ankul",
+    price : 999
+}
+function handleobject(anyobject){
+    console.log(` Username is ${anyobject.username}  and price ${anyobject.price}`)
+
+}
+//handleobject(user)
+handleobject({
+    username: "sam",
+    price : 399
+})
+
+const myNewArray =[200,100,300,440]
+
+function returnSecondValue(getArray){
+return getArray[1]
+}
+console.log (returnSecondValue(myNewArray))
